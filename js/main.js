a3325b971d55adead6b6419ae8065e3ecdd9ccb8
zwrk <92732459+zwrk@users.noreply.github.com>
2022-02-09 17:40:44 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function () {
+
+    $('input[type="tel"]').mask('+7 (999) 999-9999');
+
+    var cardsSlider = new Swiper('.cards-slider', {
+        slidesPerView: 1,
+        spaceBetween: 8,
+        speed: 600,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'progressbar',
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 16
+            },
+            992: {
+                spaceBetween: 32,
+                slidesPerView: 'auto',
+            },
+            1200: {
+                spaceBetween: 32,
+                slidesPerView: 'auto',
+            },
+        }
+    });
+
+});
